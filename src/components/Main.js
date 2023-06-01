45849c76fcb6451ffe55f5445b4fc3f294310d44
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-05-31 23:01:12 -0400

    feat: added an icon to the button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FaPlus } from 'react-icons/fa';
 import './Main.css';
 
 export default class Main extends Component {
@@ -18,12 +19,11 @@ export default class Main extends Component {
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <br />
-        <p>{novaTarefa}</p>
-
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
