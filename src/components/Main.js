b01137e6733fd4742cf5a928b84fc9c956a4f096
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-02 11:39:49 -0400

    feat: saving tasks in localStorege

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,22 @@ export default class Main extends Component {
     index: -1,
   };
 
+  componentDidMount() {
+    const { tarefas } = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({tarefas})
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }
+
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
